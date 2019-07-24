f4bdce98548d35a8b9e1bf04e16c141f63bca2bd
Adron Hall <adronhall@gmail.com>
2019-07-24 15:52:33 -0700

    Fixing the keyspace for the schema migrate process.

--- schema-migrations/up.sh
+++ schema-migrations/up.sh
@@ -3,8 +3,6 @@
 IP="172.19.0.10"
 
 echo 'Working against '${IP}'. Starting Apache Cassandra Schema Migrations.'
-#cqlsh -f 'cassandra/inception.cql'  ${IP}
-#cqlsh -f 'cassandra/20190325_create_table.up.cql' ${IP}
 
 echo "migrate -source file://cassandra/ -database cassandra://$IP/twitz up"
 migrate -source file://cassandra/ -database cassandra://$IP/twitz up
