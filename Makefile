a80979493be4b5d6eae2f12a1a9a6a07a1c1842f
Adron Hall <adronhall@gmail.com>
2019-07-25 17:11:06 -0700

    Got everything working with a single `make` command!

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 cassie-run:
 	docker-compose up -d
 	go build -o twitz
+	./migrate-up.sh
 
 cassie-stop:
 	docker-compose down
--- migrate-up.sh
+++ migrate-up.sh
@@ -2,6 +2,8 @@
 
 IP="172.22.0.5"
 
+./cassierunner -a $IP -q "create keyspace if not exists twitz with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
+
 echo 'Working against '${IP}'. Starting Apache Cassandra Schema Migrations.'
 
 echo "migrate -source file://migrations/ -database cassandra://$IP/twitz up"
